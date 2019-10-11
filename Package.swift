53f34976472bc1729fffbbeb0180f3b9bfa808a5
Gabriel O'Flaherty-Chan <hi@gabrieloc.com>
2019-10-11 15:56:54 -0400

    reorganize source for SPM

--- Package.swift
+++ Package.swift
@@ -5,10 +5,19 @@ import PackageDescription
 
 let package = Package(
   name: "Roundrect",
+  products: [
+    .library(
+      name: "Roundrect",
+      targets: ["Roundrect"])
+  ],
   targets: [
     .target(
       name: "Roundrect",
       dependencies: []
-    )
+    ),
+    .testTarget(
+      name: "RoundrectTests",
+      dependencies: ["Roundrect"],
+      path: "RoundrectTests")
   ]
 )
